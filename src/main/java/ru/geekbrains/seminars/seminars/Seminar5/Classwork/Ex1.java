369ce0f77745215b0a102ee3d1476ff0b0f5a60b
Guillaumette <anna_kniazeva@bk.ru>
2023-06-23 17:13:11 +0300

    classwork sem5 task 1 count each symbol in string

---
+++ src/main/java/ru/geekbrains/seminars/seminars/Seminar5/Classwork/Ex1.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.seminars.seminars.Seminar5.Classwork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // Посчитать количество вхождений каждого символа в текст.
+        Map<Character, Integer> mapCh = new HashMap<>();
+        String str = "бил дебила бодибилдер данила";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                if (!mapCh.containsKey(str.charAt(i))) {
+                    mapCh.put(str.charAt(i), 1);
+                } else {
+                    mapCh.put(str.charAt(i), mapCh.get(str.charAt(i)) + 1);
+                }
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : mapCh.entrySet()) {
+            System.out.println(entry.getKey() + " встретилась: " + entry.getValue() + " раз(а)");
+        }
+    }
+}
