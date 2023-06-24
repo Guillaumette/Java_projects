52d55049cd6e15fd43119e0cf7a097a11fd20bc7
Guillaumette <anna_kniazeva@bk.ru>
2023-06-24 20:24:42 +0300

    homework sem5 task 1 count target word in string

---
+++ src/main/java/ru/geekbrains/seminars/seminars/Seminar5/Homework/Task1.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.seminars.seminars.Seminar5.Homework;
+
+import java.io.BufferedReader;
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = sc.nextLine().toLowerCase().trim();
+        str = str.replaceAll("[-|–|—]|\\p{Punct}", " ");
+        while (str.contains("  ")) {
+            str = str.replace("  ", " ");
+        }
+        System.out.println("Введите искомое слово: ");
+        String targetWord = sc.nextLine();
+        countWords(str, targetWord);
+    }
+
+    private static void countWords(String str, String targetWord) {
+        String[] arrStr = str.split(" ");
+        Map<String, Integer> result = new HashMap<>();
+        for (String word : arrStr) {
+            if (!targetWord.toLowerCase().equals((word.toLowerCase()))) {
+                continue;
+            }
+            int count = result.getOrDefault(targetWord.toLowerCase(), 0);
+            result.put(word.toLowerCase(), ++count);
+        }
+        System.out.println(targetWord + " - " + result.getOrDefault(targetWord.toLowerCase(), 0));
+    }
+}
+
+
+
